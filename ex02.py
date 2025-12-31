d68955302d8ed6cf0e9edb68b71d136586485ab0
Raylan Araujo <raylanra@gmail.com>
2025-12-31 12:46:44 -0300

    Create ex02.py

---
+++ ex02.py
@@ -0,0 +1,6 @@
+# crie um programa que responda o usuário
+
+nome = input('\nEscreva seu nome: ')
+print('\nSeja bem-vindo,' + nome)
+print('{} Essa é outra forma de concatenar string e variavel'.format(nome))
+print('É um enorme prazer te conhecer, {}\n'.format(nome))
